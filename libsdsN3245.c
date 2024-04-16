511d329e77bf8b61f2a5c94013815ef5364a4a4a
Danya <dan.shell@bk.ru>
2024-04-16 23:56:29 +0300

    add comments to lib

--- libsdsN3245.c
+++ libsdsN3245.c
@@ -1,4 +1,3 @@
-
 #define _XOPEN_SOURCE 500
 #include <stdio.h>
 #include <string.h>
@@ -8,9 +7,11 @@
 
 #include "plugin_api.h"
 
-static char *g_purpose = "Check if file contains specified bytes in any order";
+// Plugin purpose and author information
+static char *g_purpose = "Check if a file contains specified bytes in any order";
 static char *g_author = "Belyakov Nikita";
 
+// Supported plugin options
 static struct plugin_option g_options[] = {
     {
         {"bytes", required_argument, 0, 0},
@@ -18,16 +19,20 @@ static struct plugin_option g_options[] = {
     }
 };
 
+// Number of supported plugin options
 static int g_options_len = sizeof(g_options) / sizeof(g_options[0]);
 
+// Function to retrieve plugin information
 int plugin_get_info(struct plugin_info *ppi)
 {
+    // Check for valid argument
     if (!ppi)
     {
         fprintf(stderr, "ERROR: Invalid argument\n");
         return -1;
     }
 
+    // Assign plugin purpose, author, and supported options
     ppi->plugin_purpose = g_purpose;
     ppi->plugin_author = g_author;
     ppi->sup_opts_len = g_options_len;
@@ -36,19 +41,23 @@ int plugin_get_info(struct plugin_info *ppi)
     return 0;
 }
 
+// Function to process a file for specified bytes
 int plugin_process_file(const char *fname,
                         struct option in_opts[],
                         size_t in_opts_len)
 {
+    // Check for valid arguments
     if (!fname || !in_opts || !in_opts_len)
     {
         errno = EINVAL;
         return -1;
     }
 
+    // Initialize variables for byte search
     char *bytes_string = NULL;
     for (size_t i = 0; i < in_opts_len; i++)
     {
+        // Extract bytes string from plugin options
         if (!strcmp(in_opts[i].name, "bytes"))
         {
             bytes_string = strdup((char *)in_opts[i].flag);
@@ -59,17 +68,20 @@ int plugin_process_file(const char *fname,
             return -1;
         }
     }
+    // Check if bytes string is valid
     if (!bytes_string)
     {
         errno = EINVAL;
         return -1;
     }
 
+    // Initialize variables for byte parsing
     unsigned char *bytes = NULL;
     size_t bytes_cnt = 0;
     char *saveptr = NULL;
     char *tok = strtok_r(bytes_string, ",", &saveptr);
     while (tok != NULL) {
+        // Parse binary format bytes
         if (strlen(tok) > 2 && tok[0] == '0' && tok[1] == 'b') {
             if (strlen(tok) > 10) {
                 errno = ERANGE;
@@ -89,6 +101,7 @@ int plugin_process_file(const char *fname,
             
             bytes_cnt++;
         }
+        // Parse hexadecimal format bytes
         else if (strlen(tok) > 2 && tok[0] == '0' && tok[1] == 'x') {
             if(strlen(tok) > 4){
                 errno = ERANGE;
@@ -104,6 +117,7 @@ int plugin_process_file(const char *fname,
             
             bytes_cnt++;
         }
+        // Parse decimal format bytes
         else
         {
             if(tok != NULL && tok[0] == '0' && strcmp(tok, "0")!= 0){
@@ -142,6 +156,7 @@ int plugin_process_file(const char *fname,
         tok = strtok_r(NULL, ",", &saveptr);
     }
 
+    // Initialize variables for byte search result
     int *found = calloc(sizeof(int), bytes_cnt);
     FILE *f = fopen(fname, "rb");
     if(!f){
@@ -170,20 +185,21 @@ int plugin_process_file(const char *fname,
         for(size_t i = 0; i < bytes_cnt; i++){
             if(memcmp((char*)bytes+i, (char*)buf, 1) == 0){
                 found[i]++;
-
-
             }
         }
     }
+    // Check if all bytes are found in the file
     int ret = 0;
     for(size_t i = 0; i < bytes_cnt; i++){
         if(found[i] == 0) ret = 1;
     }
+    // Print debug information if LAB1DEBUG environment variable is set
     if(getenv("LAB1DEBUG")!=NULL && ret == 0){
         fprintf(stderr,"Debug mode: Target bytes (");
         for(size_t i = 0; i < bytes_cnt; i++) fprintf(stderr, "%d,", (int)bytes[i]);
         fprintf(stderr, ") found in file %s!\n", fname);
     }
+    // Free allocated memory and close file
     if(found) free(found);
     if (bytes_string)
         free(bytes_string);
