8d7e0db91f7cb0178ebaa36c123e30946d1599c7
Danya <dan.shell@bk.ru>
2024-04-16 23:57:08 +0300

    change headers

--- libsdsN3245.c
+++ libsdsN3245.c
@@ -1,9 +1,10 @@
-#define _XOPEN_SOURCE 500
-#include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include <getopt.h>
 #include <errno.h>
+#define _XOPEN_SOURCE 500
+#include <stdio.h>
+#include <string.h>
+
 
 #include "plugin_api.h"
 
